f252c1e337e7fddb4e9463d653d3542f60a22938
André Ribeiro <andremezzaliraribeiro@gmail.com>
2025-09-03 01:30:22 -0300

    update: saves courses data in a JSON file
    improves performance for multiple atempts to test the script

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from io import StringIO
+import json
 import time
 # from database_service import DatabaseService
 
 BASE_URL = 'https://www1.ufrgs.br/PortalEnsino/graduacaoprocessoseletivo/DivulgacaoDadosChamamento'
+DATA_JSON_FILE = 'output.json'
 SISU = 'S'
 VESTIBULAR = 1
 MAX_RETRIES = 3
@@ -50,7 +52,7 @@ def get_courses_from_html(url, form_data):
     return options_dict
         
 
-def get_courses_data():
+def save_courses_data():
     url = f"{BASE_URL}/carregaCursos"
     years = list(range(2016, 2026))
     data = {'data': []}
@@ -85,7 +87,19 @@ def get_courses_data():
                 'course_name': sisu_data[key] 
         })
     
+    with open(DATA_JSON_FILE, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
     return data
+
+def get_courses_data():
+    try:
+        with open(DATA_JSON_FILE) as json_data:
+            d = json.load(json_data)
+            return d
+    except Exception:
+        return save_courses_data()
+
 
 def get_candidates_data(year, type , course_code):
     url = f"{BASE_URL}/carregaDadosDivulgacao"
@@ -116,7 +130,6 @@ def get_candidates_data(year, type , course_code):
     return {}
 
 courses_data = get_courses_data()
-
 for course_data in courses_data['data']:
     type = types_dict[course_data['type']]
     candidate_data = get_candidates_data(course_data['year'], type, course_data['course_code'])
